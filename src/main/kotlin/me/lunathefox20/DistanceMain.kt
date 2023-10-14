a656034a2f8ec7a52d645bc76c732d8119483a45
LunaTheFox20 <80362356+LunaTheFox20@users.noreply.github.com>
2023-10-15 01:06:59 +0200

    Update DistanceMain.kt

--- src/main/kotlin/me/lunathefox20/DistanceMain.kt
+++ src/main/kotlin/me/lunathefox20/DistanceMain.kt
@@ -3,9 +3,20 @@ package me.lunathefox20
 import me.lunathefox20.command.DistanceCalculator
 import net.weavemc.loader.api.ModInitializer
 import net.weavemc.loader.api.command.CommandBus
+import org.apache.logging.log4j.LogManager
 
 class DistanceMain : ModInitializer {
+    private val logger = LogManager.getLogger(DistanceCalculator::class.java)
+
     override fun preInit() {
-        CommandBus.register(DistanceCalculator())
+        // Log the "ready" message in preInit()
+        logger.info("DistanceCalculator is loading...")
+
+        try {
+            CommandBus.register(DistanceCalculator())
+            logger.info("DistanceCalculator is ready!")
+        } catch (e: Exception) {
+            logger.error("An error occurred during initialization:", e)
+        }
     }
 }
